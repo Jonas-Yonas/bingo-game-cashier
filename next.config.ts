c94760d2d34423b00c3c9b9401cf4a3e7950facf
@yonas_gashaw <Yonas1520@gmail.com>
2025-05-07 23:37:31 +0300

    Bug fix on dev

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,13 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com"], // Allow Google auth images
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      require("child_process").execSync("prisma generate");
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
